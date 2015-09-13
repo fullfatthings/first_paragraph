28db1d6d9d56ac31a61caa1e6c1519eefde6b958
Nicholas Thompson <njt1982@gmail.com>
2015-09-13 15:08:41 +0100

    Got the plugin working in the UI

--- src/Plugin/Field/FieldFormatter/TextFirstPara.php
+++ src/Plugin/Field/FieldFormatter/TextFirstPara.php
@@ -5,25 +5,43 @@
 
 namespace Drupal\first_paragraph\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Component\Utility\Html;
+
 
 /**
  * @FieldFormatter(
  *  id = "text_first_para",
  *  label = @Translation("First Paragraph"),
- *  field_types = {"text&quot;, &quot;text_long&quot;, &quot;text_with_summary"}
+ *  field_types = {
+ *     "text",
+ *     "text_long",
+ *     "text_with_summary"
+ *  }
  * )
  */
 class TextFirstPara extends FormatterBase {
 
   /**
-   * @FIXME
-   * Move all logic relating to the text_first_para formatter into this
-   * class. For more information, see:
-   *
-   * https://www.drupal.org/node/1805846
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterInterface.php/interface/FormatterInterface/8
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterBase.php/class/FormatterBase/8
+   * {@inheritdoc}
    */
+  public function viewElements(FieldItemListInterface $items) {
+    $elements = array();
+
+    foreach ($items as $delta => $item) {
+      $first_para = Html::load($item->value)
+        ->getElementsByTagName('p')
+        ->item(0)
+        ->cloneNode(TRUE);
+
+      $newdom = new \DOMDocument();
+      $newdom->appendChild($newdom->importNode($first_para, TRUE));
+
+      $elements[$delta] = array('#markup' => $newdom->saveHTML());
+    }
+
+    return $elements;
+  }
 
 }
